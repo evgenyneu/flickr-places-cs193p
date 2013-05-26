520fd110158334be9626d688c6897cb61c7d7d7a
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2013-05-26 19:06:58 +1000

    Reload recent photos view on viewWillAppear

--- FlickrPlaces/RecentPhotosTableViewController.m
+++ FlickrPlaces/RecentPhotosTableViewController.m
@@ -13,13 +13,25 @@
 
 #define RECENT_PHOTO_CELL_IDENTIFIER @"Recent Photo"
 
+@interface RecentPhotosTableViewController()
+
+@property (nonatomic, strong) NSArray* photos;
+
+@end
+
 @implementation RecentPhotosTableViewController
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    self.photos = [[RecentPhotosStorage getRecentPhotos] copy];
+    [self.tableView reloadData];
+}
 
 #pragma mark - Table view data source
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return [RecentPhotosStorage getRecentPhotos].count;
+    return self.photos.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -27,7 +39,7 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:RECENT_PHOTO_CELL_IDENTIFIER forIndexPath:indexPath];
     
     
-    NSDictionary *photo = [RecentPhotosStorage getRecentPhotos][indexPath.row];
+    NSDictionary *photo = self.photos[indexPath.row];
     
     NSString *description = [photo valueForKeyPath:FLICKR_PHOTO_DESCRIPTION];
     NSString *title = photo[FLICKR_PHOTO_TITLE];
